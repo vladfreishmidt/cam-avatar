c6207b757001f94f1fed9847f46f9364d25391ea
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2022-12-09 22:27:33 +0200

    Add getCoords function

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,12 +1,37 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
+ctx.translate(canvas.width / 2, canvas.height / 2);
+ctx.scale(canvas.width * 0.5, canvas.height * 0.5);
+
 canvas.width = 300;
 canvas.height = 300;
 
-canvas.addEventListener('click', (e) => {
-    const coords = getCoords(e);
+canvas.addEventListener('click', (evt) => {
+    const coords = getCoords(evt, canvas);
     console.log(coords);
 });
 
 
+const drawAvatar = () => {
+    ctx.beginPath();
+    ctx.moveTo(0, -0.77);
+    ctx.lineTo(0, 0.46);
+
+    ctx.moveTo(-0.5, -0.13);
+    ctx.lineTo(0.5, -0.13);
+
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, -0.77);
+   // ctx.quadraticCurveTo(80, 46, -0.5, -0.13);
+    //ctx.quadraticCurveTo(94, 196, 0, 220);
+    ctx.quadraticCurveTo(94, 196, 150, 220);
+
+    ctx.stroke();
+
+
+}
+
+drawAvatar();
--- scripts/utils/getCoords.js
+++ scripts/utils/getCoords.js
@@ -1,5 +1,8 @@
-const getCoords = (e) => {
-    const coords = `X: ${e.x}, Y: ${e.y}`;
+const getCoords = (evt, canvas) => {
+    const x = (evt.x - canvas.width / 2) / (canvas.width / 2);
+    const y = (evt.y - canvas.height / 2) / (canvas.height / 2);
+
+    const coords = `x: ${x.toFixed(2)}, y: ${y.toFixed(2)}`
  
     return coords;
 }
